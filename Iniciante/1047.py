7af84cc6bdd87e7dc9aed7f1092553ffb64718e3
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-07-01 19:29:59 -0300

    1047

--- Iniciante/1047.py
+++ Iniciante/1047.py
@@ -3,13 +3,13 @@ temp = list(map(int, input().split()))
 horai = temp[0] * 60 + temp[1]
 horaf = temp[2] * 60 + temp[3]
 
-horas = int(abs(horai - horaf) / 60)
-minutos = abs((horai - horaf)) % 60
+duracao = horaf -horai
 
-print(horas)
-print(minutos)
+if duracao <= 0:
+    duracao += 24 *60
 
-if horai >= horaf:
-    horas += 24
+horas = duracao // 60
+minutos = duracao % 60
 
-print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+
+print(f"O JOGO DUROU {abs(horas)} HORA(S) E {abs(minutos)} MINUTO(S)")
