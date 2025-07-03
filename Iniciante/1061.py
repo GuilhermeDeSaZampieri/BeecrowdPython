2b2ffe9a07bbde19117f86caa62b7a46e7f64435
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-07-02 23:57:56 -0300

    add 1061

--- Iniciante/1061.py
+++ Iniciante/1061.py
@@ -3,16 +3,19 @@ hms = list( input().split(" : "))
 dia2 = list( input().split())
 hms2 = list( input().split(" : "))
 
-duracaoi = int(hms[0]) * 60 + int(hms[1])
-duracaof = int(hms2[0]) * 60 + int(hms[1])
+duracaoi = int(hms[0]) * 3600 + int(hms[1]) * 60 + int(hms[2])
+duracaof = int(hms2[0]) * 3600 + int(hms2[1]) * 60 + int(hms2[2])
 
-duracao = duracaoi - duracaof
+duracao = duracaof - duracaoi
+dias = abs(int(dia[1])-int(dia2[1]))
+minutos = (duracao % 3600) // 60
 
 if duracao <= 0:
-    duracao += 24*60
+    duracao += 24*3600
+    dias -= 1
 
-horas =  duracao // 60
-minutos = duracao % 60
-segundos = abs(int(hms[2]) - int(hms2[2]))
 
-print(f"{abs(int(dia[1])-int(dia2[1]))} dia(s)\n{horas} hora(s)\n{minutos} minuto(s)\n{segundos} segundo(s)")
+horas =  duracao // 3600
+segundos = duracao % 60
+
+print(f"{dias} dia(s)\n{horas} hora(s)\n{minutos} minuto(s)\n{segundos} segundo(s)")
